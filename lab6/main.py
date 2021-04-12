b4bec3a8e5751ab0bcd4fdce87fed870bb59bd66
OverldAndrey <andrey3000.99@mail.ru>
2021-04-13 02:30:23 +0300

    complete basic app

--- lab6/main.py
+++ lab6/main.py
@@ -2,7 +2,8 @@ import sys
 
 import numpy as np
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QLineEdit
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QLineEdit, QComboBox, QHeaderView, QTableWidgetItem
 
 params_table = {
     'RELY': [0.75, 0.86, 1.0, 1.15, 1.40],
@@ -21,7 +22,6 @@ params_table = {
     'TOOL': [1.24, 1.10, 1.0, 0.91, 0.82],
     'SCED': [1.23, 1.08, 1.0, 1.04, 1.10],
 }
-
 
 project_modes = {
     'c1': [3.2, 3.0, 2.8],
@@ -48,27 +48,50 @@ class MainWindow(QDialog):
         super(MainWindow, self).__init__(parent)
         self.ui = uic.loadUi("lab6.ui", self)
 
-        self.RELY = self.ui.comboBox_1
-        self.DATA = self.ui.comboBox_2
-        self.CPLX = self.ui.comboBox_3
-        self.TIME = self.ui.comboBox_4
-        self.STOR = self.ui.comboBox_5
-        self.VIRT = self.ui.comboBox_6
-        self.TURN = self.ui.comboBox_7
-        self.ACAP = self.ui.comboBox_8
-        self.AEXP = self.ui.comboBox_9
-        self.PCAP = self.ui.comboBox_10
-        self.VEXP = self.ui.comboBox_11
-        self.LEXP = self.ui.comboBox_12
-        self.MODP = self.ui.comboBox_13
-        self.TOOL = self.ui.comboBox_14
-        self.SCED = self.ui.comboBox_15
+        self.RELY: QComboBox = self.ui.comboBox_1
+        self.DATA: QComboBox = self.ui.comboBox_2
+        self.CPLX: QComboBox = self.ui.comboBox_3
+        self.TIME: QComboBox = self.ui.comboBox_4
+        self.STOR: QComboBox = self.ui.comboBox_5
+        self.VIRT: QComboBox = self.ui.comboBox_6
+        self.TURN: QComboBox = self.ui.comboBox_7
+        self.ACAP: QComboBox = self.ui.comboBox_8
+        self.AEXP: QComboBox = self.ui.comboBox_9
+        self.PCAP: QComboBox = self.ui.comboBox_10
+        self.VEXP: QComboBox = self.ui.comboBox_11
+        self.LEXP: QComboBox = self.ui.comboBox_12
+        self.MODP: QComboBox = self.ui.comboBox_13
+        self.TOOL: QComboBox = self.ui.comboBox_14
+        self.SCED: QComboBox = self.ui.comboBox_15
+
+        self.RELY.currentIndexChanged.connect(self.calculate_project)
+        self.DATA.currentIndexChanged.connect(self.calculate_project)
+        self.CPLX.currentIndexChanged.connect(self.calculate_project)
+        self.TIME.currentIndexChanged.connect(self.calculate_project)
+        self.STOR.currentIndexChanged.connect(self.calculate_project)
+        self.VIRT.currentIndexChanged.connect(self.calculate_project)
+        self.TURN.currentIndexChanged.connect(self.calculate_project)
+        self.ACAP.currentIndexChanged.connect(self.calculate_project)
+        self.AEXP.currentIndexChanged.connect(self.calculate_project)
+        self.PCAP.currentIndexChanged.connect(self.calculate_project)
+        self.VEXP.currentIndexChanged.connect(self.calculate_project)
+        self.LEXP.currentIndexChanged.connect(self.calculate_project)
+        self.MODP.currentIndexChanged.connect(self.calculate_project)
+        self.TOOL.currentIndexChanged.connect(self.calculate_project)
+        self.SCED.currentIndexChanged.connect(self.calculate_project)
 
         self.SIZE: QLineEdit = self.ui.sizeEdit
 
-        self.project_mode = self.ui.comboBox_16
+        self.SIZE.textEdited.connect(self.calculate_project)
 
-        print(self.ui.comboBox_1.currentIndex())
+        self.project_mode: QComboBox = self.ui.comboBox_16
+
+        self.project_mode.currentIndexChanged.connect(self.calculate_project)
+
+        self.ui.wbsTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.ui.classicTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        self.calculate_project()
 
     def eaf(self):
         RELY = params_table['RELY'][self.RELY.currentIndex()]
@@ -83,11 +106,11 @@ class MainWindow(QDialog):
         PCAP = params_table['PCAP'][self.PCAP.currentIndex()]
         VEXP = params_table['VEXP'][self.VEXP.currentIndex()]
         LEXP = params_table['LEXP'][self.LEXP.currentIndex()]
-        MOPD = params_table['MOPD'][self.MODP.currentIndex()]
+        MODP = params_table['MODP'][self.MODP.currentIndex()]
         TOOL = params_table['TOOL'][self.TOOL.currentIndex()]
         SCED = params_table['SCED'][self.SCED.currentIndex()]
 
-        return RELY * DATA * CPLX * TIME * STOR * VIRT * TURN * ACAP * AEXP * PCAP * VEXP * LEXP * MOPD * TOOL * SCED
+        return RELY * DATA * CPLX * TIME * STOR * VIRT * TURN * ACAP * AEXP * PCAP * VEXP * LEXP * MODP * TOOL * SCED
 
     def PM(self):
         mode = self.project_mode.currentIndex()
@@ -99,6 +122,41 @@ class MainWindow(QDialog):
         mode = self.project_mode.currentIndex()
 
         return project_modes['c2'][mode] * (self.PM() ** project_modes['p2'][mode])
+
+    def calculate_project(self):
+        try:
+            float(self.SIZE.text())
+        except:
+            return
+
+        pm_clean = round(self.PM(), 2)
+        tm_clean = round(self.TM(), 2)
+        pm = round(pm_clean * 1.08, 2)
+        tm = round(tm_clean * 1.36, 2)
+
+        self.ui.pmLabel.setText(f'Трудоемкость: {pm}')
+        self.ui.tmLabel.setText(f'Время разработки: {tm}')
+
+        for i in range(8):
+            self.ui.wbsTable.setItem(i, 1,
+                                     QTableWidgetItem(
+                                         str(round(pm * int(self.ui.wbsTable.item(i, 0).text()) / 100.0, 2))))
+        self.ui.wbsTable.setItem(8, 1, QTableWidgetItem(str(pm)))
+
+        self.ui.classicTable.setItem(0, 0, QTableWidgetItem(str(round(pm_clean * 0.08, 2))))
+        self.ui.classicTable.setItem(1, 0, QTableWidgetItem(str(round(pm_clean * 0.18, 2))))
+        self.ui.classicTable.setItem(2, 0, QTableWidgetItem(str(round(pm_clean * 0.25, 2))))
+        self.ui.classicTable.setItem(3, 0, QTableWidgetItem(str(round(pm_clean * 0.26, 2))))
+        self.ui.classicTable.setItem(4, 0, QTableWidgetItem(str(round(pm_clean * 0.31, 2))))
+        self.ui.classicTable.setItem(5, 0, QTableWidgetItem(str(round(pm_clean, 2))))
+        self.ui.classicTable.setItem(6, 0, QTableWidgetItem(str(round(pm, 2))))
+        self.ui.classicTable.setItem(0, 1, QTableWidgetItem(str(round(tm_clean * 0.36, 2))))
+        self.ui.classicTable.setItem(1, 1, QTableWidgetItem(str(round(tm_clean * 0.36, 2))))
+        self.ui.classicTable.setItem(2, 1, QTableWidgetItem(str(round(tm_clean * 0.18, 2))))
+        self.ui.classicTable.setItem(3, 1, QTableWidgetItem(str(round(tm_clean * 0.18, 2))))
+        self.ui.classicTable.setItem(4, 1, QTableWidgetItem(str(round(tm_clean * 0.28, 2))))
+        self.ui.classicTable.setItem(5, 1, QTableWidgetItem(str(round(tm_clean, 2))))
+        self.ui.classicTable.setItem(6, 1, QTableWidgetItem(str(round(tm, 2))))
 
 
 def main():
